80cec007e15b7713ff938c05a8cfc9b4c96e6249
HassamBaigSAH <98746322+HassamBaigSAH@users.noreply.github.com>
2022-01-31 16:43:18 +0500

    first commit

---
+++ precache-manifest.9279516fb4046d0a96200ec959c2366a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10630631159388c5f59",
+    "url": "/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a1e88e62c585b41735e4281ff00ecec4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e10630631159388c5f59",
+    "url": "/js/app.b8d8e0c7.js"
+  },
+  {
+    "revision": "624a16f446a955f52556",
+    "url": "/js/chunk-vendors.a8599dca.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
